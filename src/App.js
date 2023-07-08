69a47fdaa9df09f6ed9b673873d93ce4f81fbef9
Himanshu Singh <himanshujrs6@gmail.com>
2023-07-08 11:27:51 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-import "./index.css";
-function App() {
-  return <div className="App"></div>;
+import React from 'react'
+import Navbar from './components/Navbar'
+import TodoList from "./components/TodoList";
+import { Link, BrowserRouter, Route, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <BrowserRouter>
+        <Link to="/notes" />
+        <Link to="/aboutUs" />
+        <Switch>
+          <Route path="/notes" Component={TodoList}>
+            <TodoList />
+          </Route>
+          <div>Calender</div>
+          <div>FAQs</div>
+          <div>Notes</div>
+          <div>About Us!</div>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import stb from '../constants/stb.png';
+import './Navbar.css';
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={stb} alt="app__logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">Notes</a></li>
+        <li className="p__opensans"><a href="#menu">Calendar</a></li>
+        <li className="p__opensans"><a href="#awards">FAQs</a></li>
+        <li className="p__opensans"><a href="#contact">About Us</a></li>
+      </ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">Log In</a>
+        <div />
+        <a href="/" className="p__opensans">Sign Up</a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+            <ul className="app__navbar-smallscreen_links">
+              <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
+              <li><a href="#about" onClick={() => setToggleMenu(false)}>Notes</a></li>
+              <li><a href="#menu" onClick={() => setToggleMenu(false)}>Calendar</a></li>
+              <li><a href="#awards" onClick={() => setToggleMenu(false)}>FAQs</a></li>
+              <li><a href="#contact" onClick={() => setToggleMenu(false)}>About Us</a></li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
